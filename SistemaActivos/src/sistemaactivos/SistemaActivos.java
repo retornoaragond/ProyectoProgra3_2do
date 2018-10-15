9207f2ed5c5f37404373f3b643b7232d8a55b26c
retornoaragond <eef251195@gmail.com>
2018-10-14 21:04:42 -0600

    creacion del proyecto

---
+++ SistemaActivos/src/sistemaactivos/SistemaActivos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemaactivos;
+
+/**
+ *
+ * @author ExtremeTech
+ */
+public class SistemaActivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
